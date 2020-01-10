8e0c3d8af06cec430a0382bd6baac9bd5ee58cb9
Renan Toledo <renantcosta@gmail.com>
2020-01-10 16:23:26 -0300

    Primeiro Layout

---
+++ LojaVirtual/LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult visualizar()
+        {
+
+            return View();
+            //return new ContentResult() { Content = "<h3>Produto -> Visualizar</h3>", ContentType = "text/html" };
+        }
+    }
+}
