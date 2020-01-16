39f608247f77cb747f8f3d631e45bbf7c1333873
Renan Toledo <renantcosta@gmail.com>
2020-01-16 14:53:31 -0300

    Model Razor

--- LojaVirtual/LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
+++ LojaVirtual/LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +11,22 @@ namespace LojaVirtual.Controllers
     {
         public ActionResult visualizar()
         {
+            Produto produto = GetProduto();
 
-            return View();
+            return View(produto);
             //return new ContentResult() { Content = "<h3>Produto -> Visualizar</h3>", ContentType = "text/html" };
         }
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "Xbox One X",
+                Descricao = "Jogue em 4K",
+                Valor = 2000.00M
+            };
+        }
+
     }
 }
---
+++ LojaVirtual/LojaVirtual/LojaVirtual/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+        public Decimal Valor { get; set; }
+        
+    }
+}
